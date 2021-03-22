b9da00f42e624fde2c15bb5f15e37ca3ec970589
Lan <truongylan2000@gmail.com>
2021-03-22 12:53:59 +0700

    test - add server

---
+++ server.js
@@ -0,0 +1,21 @@
+const dotenv = require("dotenv");
+const app = require("./app");
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const key = require("./server/config/key");
+
+dotenv.config({ path: "./config.env" });
+
+const port = process.env.PORT || 8000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const connect = mongoose
+  .connect(key.MongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+}); // start the server
