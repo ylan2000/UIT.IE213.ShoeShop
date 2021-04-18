2fea5418e3b031266ce87162be73c03def1e4c96
Long Nguyễn Phúc <18521044@gm.uit.edu.vn>
2021-04-18 15:51:47 +0700

    #25 add packages: body-parser, method-override

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require("express");
 const path = require("path");
+const bodyParser = require("body-parser"); //Để lấy thông tin từ body web
+const methodOverride = require("method-override"); //Sử dụng PUT, Delete,...
 
 const adminRouter = require("./routes/viewAdminRoutes");
 const clientRouter = require("./routes/viewClientRoutes");
@@ -11,6 +13,11 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static(`${__dirname}/public`));
+
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
+
+app.use(methodOverride('_method'));
+
 
 //~~~~~~ROUTING~~~~~~~
 app.use("/admin", adminRouter);
