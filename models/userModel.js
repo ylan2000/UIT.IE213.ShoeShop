abda02f01db9a010b88af57d3d7cd99727c6ad26
Lan <truongylan2000@gmail.com>
2021-05-02 20:32:34 +0700

    #22 Update models with validator & add some fields

--- models/userModel.js
+++ models/userModel.js
@@ -1,21 +1,51 @@
 const mongoose = require("mongoose");
+const validator = require('validator');
 const schema = mongoose.Schema;
 
 const userSchema = schema({
   image: { type: Array, default: [] },
-  fullName: { type: String, maxlength: 50 },
-  userName: { type: String, trim: true, unique: 1 },
-  email: { type: String, trim: true, unique: 1 },
-  address: { type: String },
+  fullName: { type: String, required: [true, 'Please provide your name!'],},
+  userName: {
+    type: String,
+    required: [true, 'Please provide your username!'],
+    unique: true,
+    lowercase: true,
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your email!'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please a provide a valid email'],
+  },
+  address: { type: String, required: [true, "Please provide a password"] },
   phone: { type: String, maxlength: 10 },
-  password: { type: String, minglength: 5 },
+  password: {
+    type: String,
+    required: [true, "Please provide a password"],
+    minlength: 8,
+    select: false,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, "Please confirm your password"],
+    validate: {
+      // this only works on CREATE and SAVE
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: "Passwords are not the same!",
+    },
+  },
   role: { type: Number, default: 0 }, // 0: client, 1: admin
   createdDate: { type: Date, default: Date.now() },
-  avatarImage:{ type: Object, default:
-    {
-      data: {type: Buffer},
-      type: {type: String}
-    } },
+  avatarImage: {
+    type: Object,
+    default: {
+      data: { type: Buffer },
+      type: { type: String },
+    },
+  },
 });
 
 userSchema.virtual("avatarImagePath").get(function () {
