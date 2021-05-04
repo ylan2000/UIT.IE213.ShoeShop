1e83a63fcf094799df50ed65eb98c905fa056138
Long Nguyễn Phúc <18521044@gm.uit.edu.vn>
2021-05-04 11:46:35 +0700

    #1 Add body-parser

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require("express");
 const path = require("path");
-const methodOverride = require("method-override"); 
+const bodyParser = require("body-parser"); //Để lấy thông tin từ body web
+const methodOverride = require("method-override"); //Sử dụng PUT, Delete,...
+
 const adminRouter = require("./routes/viewAdminRoutes");
 const clientRouter = require("./routes/viewClientRoutes");
 
@@ -12,9 +14,14 @@ app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static(`${__dirname}/public`));
 
-app.use(methodOverride('_method')); 
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
+
+app.use(methodOverride('_method'));
+
 
 //~~~~~~ROUTING~~~~~~~
+
+//view
 app.use("/admin", adminRouter);
 app.use("/", clientRouter);
 
