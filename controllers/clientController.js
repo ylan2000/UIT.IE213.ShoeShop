b90f442cf6cf1b3c3be06ee0243094b432293de0
Long Nguyễn Phúc <18521044@gm.uit.edu.vn>
2021-05-07 20:52:52 +0700

    #28 backend payment handling

--- controllers/clientController.js
+++ controllers/clientController.js
@@ -1,8 +1,16 @@
 const {Product} = require('../models/productModel');
+const {Transaction} = require('../models/transactionModel')
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
+const axios = require("axios")
+
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY
+const stripePublicKey = process.env.STRIPE_PUBLIC_KEY
+
+const stripe = require('stripe')(stripeSecretKey)
 
 exports.getHome = async (req, res, next) => {
   try {
-    // Render template
     const products = await (await Product.find().sort({createdDate: -1})).slice(0, 8);
     return res.status(200).render("pages/home", { title: "Home", product: products});
   } catch (err) {
@@ -128,16 +136,37 @@ exports.get404 = async (req, res, next) => {
 };
 
 exports.postCheckout = async (req,res) => {
-  const total = req.body.cart__totalMoney
+  const total = "$471.25"
   try
   {
     return res.status(200).render("pages/payment", {
       title: "Checkout",
-      total: total
+      total: total,
+      stripePublicKey: stripePublicKey
     })
   }
   catch (err)
   {
 
   }
+}
+
+exports.postPaymentDone = async(req, res) => {
+  //console.log(req.body.stripeTokenId)
+  stripe.charges.create({
+    amount: req.body.total,
+    source: req.body.stripeTokenId,
+    currency: 'usd',
+  }).then(async function(){
+    const trans = new Transaction({
+      total: req.body.total,
+      status: true
+    })
+    const newTrans = await trans.save()
+    console.log('Charge Successful')
+    res.json({ message: 'Successfully purchased items' })
+  }).catch(function(err){
+    console.log(err)
+    res.status(500).end()
+  })
 }
