6ef951aa256a51bbcc2b79ee400a8683dc849b2c
Lan <truongylan2000@gmail.com>
2021-07-01 23:56:36 +0700

    #1 Errors Outside Express Unhandled Rejections

--- server.js
+++ server.js
@@ -17,9 +17,16 @@ mongoose
     useUnifiedTopology: true,
     useFindAndModify: false,
   })
-  .then(() => console.log("DB connection successful"))
-  .catch((err) => console.log(err));
+  .then(() => console.log("DB connection successful"));
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}`);
 }); // start the server
+
+process.on('unhandledRejection', err => {
+  console.log(err.name, err.message);
+
+  server.close(() => {
+    process.exit(1);
+  });
+});
