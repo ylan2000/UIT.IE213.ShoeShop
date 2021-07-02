082ea03c6fb2060dae717b9ed8bee145042c98e6
Lan <truongylan2000@gmail.com>
2021-07-02 08:44:52 +0700

    #1 Catching Uncaught Exceptions

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const dotenv = require("dotenv");
 const app = require("./app");
 
 const mongoose = require("mongoose");
+
+process.on('uncaughtException', err => {
+  console.log(err.name, err.message);
+
+  process.exit(1);
+});
+
 dotenv.config({ path: "./config.env" });
 
 const port = process.env.PORT || 8000;
