a918f15493cc9ebdae631985a0a3ea887ac35013
Lan <truongylan2000@gmail.com>
2021-07-14 10:24:16 +0700

    #44 Code for test

--- services/chatbotService.js
+++ services/chatbotService.js
@@ -49,10 +49,10 @@ let sendMessageOptions = (sender_psid) => {
               ]
           };
 
-          sendMessage(sender_psid, response1);
-          sendMessage(sender_psid, response2);
-          sendMessage(sender_psid, response3);
-          sendMessage(sender_psid, response4);
+          await sendMessage(sender_psid, response1);
+          await sendMessage(sender_psid, response2);
+          await sendMessage(sender_psid, response3);
+          await sendMessage(sender_psid, response4);
           resolve("done");
       } catch (e) {
           reject(e);
@@ -124,29 +124,34 @@ let passThreadControl = (sender_psid, app) => {
 };
 
 let sendMessage = (sender_psid, response) => {
-  console.log("in send message function=========================");
-  // Construct the message body
-  let request_body = {
-    "recipient": {
-      "id": sender_psid
-    },
-    "message": { "text": response }
-  }
+  return new Promise(async (resolve, reject) => {
+      try {
+          console.log(response + "==================================");
+          // Construct the message body
+          let request_body = {
+              "recipient": {
+                  "id": sender_psid
+              },
+              "message": response
+          };
 
-  // Send the HTTP request to the Messenger Platform
-  request({
-    "uri": "https://graph.facebook.com/v6.0/me/messages",
-    "qs": { "access_token": process.env.FB_PAGE_TOKEN },
-    "method": "POST",
-    "json": request_body
-  }, (err, res, body) => {
-    if (!err) {
-      console.log('message sent!');
-      console.log(`My messenger: ${response}`);
-    } else {
-      console.error("Unable to send message:" + err);
-    }
-  }); 
+          // Send the HTTP request to the Messenger Platform
+          request({
+              "uri": "https://graph.facebook.com/v6.0/me/messages",
+              "qs": { "access_token": PAGE_ACCESS_TOKEN },
+              "method": "POST",
+              "json": request_body
+          }, (err, res, body) => {
+              if (!err) {
+                  resolve('message sent!')
+              } else {
+                  reject("Unable to send message:" + err);
+              }
+          });
+      } catch (e) {
+          reject(e);
+      }
+  });
 };
 
 module.exports = {
