be04c701d33007982ad79ffadae68f73e4b118f9
Lan <truongylan2000@gmail.com>
2021-07-14 22:21:18 +0700

    #44 Code for test

--- controllers/chatbotController.js
+++ controllers/chatbotController.js
@@ -157,7 +157,7 @@ let handleMessage = async (sender_psid, message) => {
       if (payload.startsWith("PICK_BY_PRICE")) {
         const payloadArr = payload.split("_");
         const payloadValue = payloadArr[payloadArr.length - 1];
-        console.log(payload.substr(payloadValue + "===============3lastttttttt"));
+        console.log(payloadValue + "===============3lastttttttt");
       } else if (payload === "CATEGORIES") {
         await chatbotService.sendCategories(sender_psid);
 
