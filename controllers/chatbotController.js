e5a751c4d8842a3641a50a4db85525fcd56c2ab5
Lan <truongylan2000@gmail.com>
2021-07-14 15:14:50 +0700

    #44 Code for test

--- controllers/chatbotController.js
+++ controllers/chatbotController.js
@@ -14,17 +14,19 @@ let postWebhook = (req, res) => {
 
       if (entry.standby) {
         //if user's message is "back" or "exit", return the conversation to the bot
-        console.log(JSON.stringify(entry.standby) + "====================================");
-        // let webhook_standby = entry.standby[0];
-        // if (webhook_standby && webhook_standby.message) {
-        //     if (webhook_standby.message.text === "back" || webhook_standby.message.text === "exit") {
-        //         // call function to return the conversation to the primary app
-        //         chatbotService.takeControlConversation(webhook_standby.sender.id);
-        //     }
-        // }
+        console.log(JSON.stringify(entry.standby) + "====================================entrystandby");
+        let webhook_standby = entry.standby[0];
+        if (webhook_standby && webhook_standby.message) {
+            if (webhook_standby.message.text === "back" || webhook_standby.message.text === "exit") {
+                // call function to return the conversation to the primary app
+                chatbotService.takeControlConversation(webhook_standby.sender.id);
+            }
+        }
 
-        // return;
+        return;
       }
+
+      console.log(JSON.stringify(entry.messaging) + "====================================entrymessage");
 
       // Gets the body of the webhook event
       let webhook_event = entry.messaging[0];
